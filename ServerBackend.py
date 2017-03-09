e7a8275ac26ef8531ad9b96dc2ccf51bf87e20ca
EJ <eppingere@winchesterthurston.org>
2017-03-09 13:23:37 -0500

    Hello World + Student Class in Backend

--- ServerBackend.py
+++ ServerBackend.py
@@ -1,9 +1,28 @@
 from flask import Flask
+
 app = Flask(__name__)
+
+
+class Student():
+
+    name = ""
+    advisor = ""
+    grade = 0
+    parentEmail = ""
+
+    def __init__(self, name, grade, advisor, parentEmail):
+        self.name = name
+        self.advisor = advisor
+        self.grade = grade
+        self.parentEmail = parentEmail
+
 
 @app.route("/")
 def hello():
+    s = Student("EJ Eppinger", 12, "Nassar", "parentemail@email.com")
+    print(s)
     return "Hello World!"
+
 
 if __name__ == "__main__":
     app.run()
