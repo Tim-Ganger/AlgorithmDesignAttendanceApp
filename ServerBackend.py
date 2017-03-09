aabde24f4742ac7d5d2bdfa6ed9a7287e2e92dc1
nosticks <mossn@winchesterthurston.org>
2017-03-09 14:04:05 -0500

    Make Student Class Work with Databases

--- ServerBackend.py
+++ ServerBackend.py
@@ -1,14 +1,19 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
+URI = "sqlite:////tmp/test.db"
+app.config['SQLALCHEMY_DATABASE_URI'] = URI
+db = SQLAlchemy(app)
 
-class Student():
+class Student(db.Model):
 
-    name = ""
-    advisor = ""
-    grade = 0
-    parentEmail = ""
+    __tablename__ = 'UserRemap'
+    name = db.Column(db.String, primary_key=True)
+    advisor = db.Column(db.String)
+    grade = db.Column(db.Integer)
+    parentEmail = db.Column(db.String)
 
     def __init__(self, name, grade, advisor, parentEmail):
         self.name = name
@@ -16,12 +21,23 @@ class Student():
         self.grade = grade
         self.parentEmail = parentEmail
 
+    def __repr__(self):
+        return "Student Info: " + self.name + ", " + self.advisor
+
+    @property
+    def json(self):
+        return {
+            "name": self.name,
+            "advisor": self.advisor,
+            "grade": self.grade,
+            "parentEmail": self.parentEmail
+        }
+
 
 @app.route("/")
 def hello():
     s = Student("EJ Eppinger", 12, "Nassar", "parentemail@email.com")
-    print(s)
-    return "Hello World!"
+    return s.__repr__()
 
 
 if __name__ == "__main__":
